3f5e5fd863332c5a69cc9c23c81d9fe4f657deae
joperezpe <94140446+joperezpe@users.noreply.github.com>
2021-12-17 11:08:11 -0500

    creacion pilas y colas
    se crean las pilas para los contenedores y  las colas para los vehiculos

---
+++ Creacionlistasycolas.java
@@ -0,0 +1,18 @@
+ static void obtenerElementosPilaContenedores(ArrayList<LinkedList> contenedores, int consecutivo){
+            System.out.println();
+            System.out.println("La pila con indice " + (consecutivo-1) + ", Tiene los siguientes contenedores:");
+            LinkedList<Integer> pila = contenedores.get(consecutivo-1);
+            Iterator<Integer> iterador = pila.listIterator();
+            while(iterador.hasNext()){
+                System.out.println("Serial: " + iterador.next());
+            }
+        }
+        static void obtenerElementosColaVehiculos(ArrayList<LinkedList> vehiculos, int consecutivo){
+            System.out.println();
+            System.out.println("La cola con indice " + (consecutivo-1) + ", Tiene los siguientes vehiculos:");
+            LinkedList<Integer> pila = vehiculos.get(consecutivo-1);
+            Iterator<Integer> iterador = pila.listIterator();
+            while(iterador.hasNext()){
+                System.out.println("Serial: " + iterador.next());
+            }
+        }
